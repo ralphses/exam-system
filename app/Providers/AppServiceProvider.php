c37601bc3c134bdd369b41c5e5523a8eab5aeaf0
Raphael Eze <eze.raph@gmail.com>
2023-03-31 06:03:32 +0100

    Fixed minor bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
         if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
             
-            chmod(Storage::path("students"), 775);
+            chmod("/app", 775);
 
         }
 
