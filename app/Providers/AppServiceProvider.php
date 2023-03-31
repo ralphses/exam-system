9ef95c9bf75f097ed7a3931cafa6959e72c6aaec
Raphael Eze <eze.raph@gmail.com>
2023-03-31 05:48:28 +0100

    Fixed bugs with storage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,5 +28,7 @@ class AppServiceProvider extends ServiceProvider
         if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
+
+        chmod("../public/storage", 775);
     }
 }
